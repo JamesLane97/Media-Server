a44e0fbf508ae323a6fb5975975494c13856ea68
James Lane <jameslane1997@live.co.uk>
2024-02-16 00:23:46 +0000

    Added docker and required package update and install script

---
+++ update_and_install_packages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update package lists and upgrade installed packages
+sudo apt update && sudo apt upgrade -y
+
+# Install required packages
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+# Add Docker's official GPG key
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod 644 /etc/apt/keyrings/docker.asc
+
+# Add the Docker repository to Apt sources
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+# Install Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Run Docker Hello World to verify installation
+sudo docker run hello-world
+
+echo "Docker & required packages installed"
